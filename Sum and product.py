580dbaa3bf920f62ce96966271314cea41915945
rylle643 <2023-2-01900@lpunetwork.edu.ph>
2024-08-31 11:45:33 +0800

    Add files via upload

---
+++ Sum and product.py
@@ -0,0 +1,15 @@
+#Initialization for the value of sum and product
+sum = 0
+product = 0
+
+#Input the value of the first and second number
+first_number = int(input("Enter the value of the first number:"))
+second_number = int (input("Enter the value of the second number:"))
+
+#Setting formula for the computation of sum and product
+sum = first_number + second_number
+product = first_number * second_number
+
+#Display the computed amount for sum and product
+print("The sum of two numbers is:", sum)
+print("The product of two number is:", product)
